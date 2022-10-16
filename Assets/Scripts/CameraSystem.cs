6085d8720646aef898a06a865d3c83e3b4fb8fbf
maidmage <46210759+glockmage@users.noreply.github.com>
2022-10-16 00:39:52 -0400

    camera works fairly well

---
+++ Assets/Scripts/CameraSystem.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://www.youtube.com/watch?v=pJQndtJ2rk0&ab_channel=CodeMonkey
+
+public class CameraSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 inputDir = new (0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W)) inputDir.z = +1f;
+        if (Input.GetKey(KeyCode.S)) inputDir.z = -1f;
+        if (Input.GetKey(KeyCode.A)) inputDir.x = -1f;
+        if (Input.GetKey(KeyCode.D)) inputDir.x = +1f;
+
+        Vector3 moveDir = transform.forward * inputDir.z + transform.right * inputDir.x;
+
+        float moveSpeed = 15f;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        float rotateDir = 0f;
+        if (Input.GetKey(KeyCode.Q)) rotateDir = +1f;
+        if (Input.GetKey(KeyCode.E)) rotateDir = -1f;
+
+        float rotateSpeed = 100f;
+        transform.eulerAngles += new Vector3(0, rotateDir * rotateSpeed * Time.deltaTime, 0);
+
+
+    }
+}
